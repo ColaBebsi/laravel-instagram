d6b33b967a062e70300c06615636be678e6f1b1d
Moohammad Samaae <moohammad.samaae@bonnierpharmainsights.se>
2020-10-31 22:10:15 +0100

    Generated login flow, designed the UI, adding username register flow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,43 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row justify-content-md-center">
+        <div class="col-md-3 pl-5">
+            <img src="{{URL::asset('/images/img3.jpg')}}" alt="" class="rounded-circle w-100">
+        </div>
+        <div class="col-md-9">
+            <h1>{{ Auth::user()->name }}</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+            <div class="d-flex mb-4">
+                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>123</strong> followers</div>
+                <div class="pr-5"><strong>123</strong> following</div>
+            </div>
+            <div>
+                Profile Title - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, laudantium?
+                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+            </div>
+            <div>Profile description</div>
+            <div><a href="#">www.test.test</a></div>
+        </div>
+    </div>
+</div>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="{{URL::asset('/images/img1.jpg')}}" alt="" class="w-100">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="{{URL::asset('/images/img2.jpg')}}" alt="" class="w-100">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="{{URL::asset('/images/img3.jpg')}}" alt="" class="w-100">
                 </div>
             </div>
         </div>
